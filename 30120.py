c035046c9ce512353feab809e0828f9de5a9e515
허준 <24y30120@pcs.hs.kr>
2024-05-03 10:07:19 +0900

    first commit

--- 30120.py
+++ 30120.py
@@ -1,9 +1,12 @@
-학번 = [3, 0, 1, 2, 0]
+import base64 as b64
+
+학번 = []
+
+학번.append(int(b64.b64decode("Mw==").decode().format("utf-8")))
+학번.append(int(b64.b64decode("MA==").decode().format("utf-8")))
+학번.append(int(b64.b64decode("MQ==").decode().format("utf-8")))
+학번.append(int(b64.b64decode("Mg==").decode().format("utf-8")))
+학번.append(int(b64.b64decode("MA==").decode().format("utf-8")))
+
 for i in 학번:
-    print(i, end="")
-
-print()
-
-이름 = ["허", "준"]
-for i in 이름:
     print(i, end="")
